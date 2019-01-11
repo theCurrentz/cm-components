ba4b55d22fb7e47896739ae08735d5d0e7df3036
Health Guide <admin@healthguide.com>
2019-01-10 17:12:27 -0800

    build form script

---
+++ components/forms/master-form.js
@@ -0,0 +1,3 @@
+'use strict'
+import "/var/www/html/wp-content/plugins/cm-components/components/forms/signup/facebook-api-init.js"
+import "/var/www/html/wp-content/plugins/cm-components/components/forms/signup/form-action.js"
